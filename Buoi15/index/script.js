2a78dc9a6cce341ae983689cdfd2f1438d75803f
BangDiep <74973421+BangDiep@users.noreply.github.com>
2021-01-05 21:54:12 +0700

    update

--- Buoi15/index/script.js
+++ Buoi15/index/script.js
@@ -1,13 +1,33 @@
 // 1. Đếm xem có tất cả bao nhiêu từ trong 2 đoạn văn có độ dài lớn hơn 8
-
+let p = document.getElementsByTagName("p");
+let countString = (str) => (str.length > 8) ? true : false;
+for (let i = 0; i < p.length; i++) {
+    let content = p[i].innerText.split(" "),count = 0;
+    console.log(content);
+    for (let j = 0; j < content.length;j++) {
+        if (countString(content[j])) {
+            count++;
+        } 
+    }
+    console.log("P" + (i+1) + " co " + count + " tu do dai lon hon 8" );
+}
 
 
 
 // 2. Style cho các từ có độ dài lớn hơn 8 trong p1
+let p1 = document.querySelector('.p1'); 
+let countString2 = (str) => (str.length > 8) ? true : false;
+let newContent2 = p1.innerText.split(" ");
 
+for (let i = 0; i < newContent2.length; i++ ) {
+    if (countString2(newContent2[i])) {
+        newContent2[i] = `<span style="color: blue"> ${newContent2[i]}</span>`
+    }
+}
+p1.innerHTML = newContent2.join(" ");
 
 // 3. Thay thế các từ có độ nhỏ hơn 8 trong p2 thành "Anh Ba đẹp trai 🤣". Lưu ý thay thế đúng không thì 0đ!!!
-
+p2.innerHTML = p2.textContent.split(" ").map(i=> i.length < 8 ? "Anh Ba đẹp trai 🤣" : i).join(" ");
 
 // 4. Thêm src, alt, href cho ảnh, link
 let image = document.body.getElementsByTagName("img")[0];
--- Buoi15/todo/todo.js
+++ Buoi15/todo/todo.js
@@ -9,4 +9,12 @@ let todos = [
 ];
 
 // Từ todolist trên render ra trang HTML theo mẫu trong ảnh
-
+let doing = document.querySelector("#doing"),
+complete = document.querySelector("#complete");
+for (let i = 0; i < todos.length; i++) {
+  if (todos[i].completed) {
+  complete.lastElementChild.innerHTML += `<div><input type="checkbox" name="${todos[i].id}" checked id="${todos[i].id}">${todos[i].title}</div>`;
+  } else {
+  doing.lastElementChild.innerHTML += `<div id='${todos[i].id}'><input type="checkbox" name="${todos[i].id}" id="${todos[i].id}">${todos[i].title}</div>`;
+}
+}
