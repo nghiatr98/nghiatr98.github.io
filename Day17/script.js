46b2020d3885ac20b0e91309c3c6bdf198840fba
BangDiep <74973421+BangDiep@users.noreply.github.com>
2021-01-08 02:32:29 +0700

    Day17

---
+++ Day17/script.js
@@ -0,0 +1,25 @@
+let slides = document.getElementsByClassName("slide"),
+    slideIndex = 1;
+let showSlides = (slideIndex) => {
+    if (slideIndex > slides.length) slideIndex = 1;
+    if (slideIndex < 1) slideIndex = slides.length;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    slides[slideIndex-1].classList.add("active");
+}
+let changeSlide = (n) => {
+    slideIndex += n;
+    if (slideIndex > slides.length) slideIndex = 1;
+    if (slideIndex < 1) slideIndex = slides.length;
+    showSlides(slideIndex);
+}
+
+showSlides(slideIndex);
+
+//dropdown
+let dropDown = document.querySelector('.relative')
+let dropDownItem = document.querySelector('.dropDown-item');
+dropDown.onclick = function () {
+    dropDownItem.classList.toggle('dropDownShow')
+}
